04335d3354ca2a41738409e4e846aae13a7b550b
unknown <arunachalam@vhitechlab.com>
2024-12-30 19:06:07 +0530

    dev: Setup Initiated

---
+++ src/app/components/shared/app-bar/app-bar.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { environment } from '../../../../environment/environment';
+import { APP_BAR_MENU } from '../../../constants/contents';
+import { AvatarComponent } from '../avatar/avatar.component';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-bar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, RouterLink, AvatarComponent],
+  templateUrl: './app-bar.component.html',
+  styleUrl: './app-bar.component.scss',
+})
+export class AppBarComponent {
+  appName = environment?.APP_NAME;
+  appBarMenu = APP_BAR_MENU;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    console.log(this.route);
+  }
+}
