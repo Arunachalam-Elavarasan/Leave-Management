e282b34664fb82f8633b46bda01115033ad46e8f
Arunachalam-Elavarasan <arunachalam@quest1.io>
2025-01-06 13:39:50 +0530

    dev: User Leave List was completed

--- src/app/pages/leave/form/form.component.ts
+++ src/app/pages/leave/form/form.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ScreenHeaderComponent } from '../../../components/shared/screen-header/screen-header.component';
+import { TextFieldComponent } from '../../../components/shared/form-fields/text-field/text-field.component';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DateFieldComponent } from '../../../components/shared/form-fields/date-field/date-field.component';
 
 @Component({
   selector: 'leave-form',
   standalone: true,
-  imports: [],
+  imports: [ScreenHeaderComponent, TextFieldComponent, DateFieldComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
-export class FormComponent {}
+export class FormComponent {
+  actions = [
+    {
+      label: 'Save',
+      action: 'save',
+      color: 'primary',
+    },
+    {
+      label: 'Cancel',
+      action: 'cancel',
+      color: 'accent',
+    },
+  ];
+
+  formBuilder = inject(FormBuilder);
+
+  leaveValue = this.formBuilder.group({
+    typeOfLeave: ['', Validators.required],
+    comment: ['', Validators.required],
+    startDate: ['', Validators.required],
+    endDate: ['', Validators.required],
+  });
+}
--- src/app/pages/leave/list/list.component.ts
+++ src/app/pages/leave/list/list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ScreenHeaderComponent } from '../../../components/shared/screen-header/screen-header.component';
+import { Store } from '@ngrx/store';
+import { getLeaveDetails } from '../../../store/app/app.selector';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'leave-list',
   standalone: true,
-  imports: [],
+  imports: [ScreenHeaderComponent, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {}
+export class ListComponent {
+  private store = inject(Store);
+  leaveDetails: any = [];
+
+  ngOnInit(): void {
+    this.store.subscribe({
+      next: (state) => {
+        this.leaveDetails = state?.app?.leaveDetails || [];
+      },
+    });
+  }
+}
